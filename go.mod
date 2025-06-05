669d89323971ac5e1740d1a5d409b337d25ecd6f
saikumaradapa <saikumaradapa3266@gmail.com>
2025-06-05 22:05:25 +0530

    add post model

--- go.mod
+++ go.mod
@@ -4,5 +4,10 @@ go 1.24.2
 
 require (
 	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 )
--- internal/store/posts.go
+++ internal/store/posts.go
@@ -5,10 +5,41 @@ import (
 	"database/sql"
 )
 
-type PostsStore struct {
+type Post struct {
+	ID        int64    `json:"id"`
+	Content   string   `json:"content"`
+	Title     string   `json:"title"`
+	UserID    int64    `json:"user_id"`
+	Tags      []string `json:"tags"`
+	CreatedAt string   `json:"created_at"`
+	UpdatedAt string   `json:"updated_at"`
+}
+
+type PostStore struct {
 	db *sql.DB
 }
 
-func (s *PostsStore) Create(ctx context.Context) error {
+func (s *PostStore) Create(ctx context.Context, post *Post) error {
+	query := `
+	INSERT INTO posts (content, title, user_id, tags)
+	VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at
+	`
+
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		post.Content,
+		post.Title,
+		post.UserID,
+		post.Tags,
+	).Scan(
+		&post.ID,
+		&post.CreatedAt,
+		&post.UpdatedAt,
+	)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
