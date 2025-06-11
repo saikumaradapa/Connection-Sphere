401b538a4b4443272a60f9814289faabd5de23c3
saikumaradapa <saikumaradapa3266@gmail.com>
2025-06-11 22:09:48 +0530

    update healthcheck handler

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -3,5 +3,7 @@ package main
 import "net/http"
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("ok"))
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte(`{"status": "ok"}`))
 }
