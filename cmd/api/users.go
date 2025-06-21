836d31dc88a44c17add96abbb69fb792f566527b
saikumaradapa <saikumaradapa3266@gmail.com>
2025-06-21 22:52:58 +0530

    add user Context Middleware

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"strconv"
 
@@ -8,29 +9,52 @@ import (
 	"github.com/saikumaradapa/Connection-Sphere/internal/store"
 )
 
+type userKey string
+
+const userCtx userKey = "user"
+
 func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
-	userID, err := strconv.ParseInt(chi.URLParam(r, "userID"), 10, 64)
-	if err != nil {
-		app.badRequestResponse(w, r, err)
-		return
-	}
-
-	ctx := r.Context()
-
-	user, err := app.store.Users.GetByID(ctx, userID)
-	if err != nil {
-		switch err {
-		case store.ErrNotFound:
-			app.notFoundResponse(w, r, err)
-			return
-		default:
-			app.internalServerError(w, r, err)
-			return
-
-		}
-	}
+	user := getUserFromCtx(r)
 
 	if err := app.jsonResponse(w, http.StatusOK, user); err != nil {
 		app.internalServerError(w, r, err)
+		return
 	}
+}
+
+func (app *application) userContextMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userID, err := strconv.ParseInt(chi.URLParam(r, "userID"), 10, 64)
+		if err != nil {
+			app.badRequestResponse(w, r, err)
+			return
+		}
+
+		ctx := r.Context()
+
+		user, err := app.store.Users.GetByID(ctx, userID)
+		if err != nil {
+			switch err {
+			case store.ErrNotFound:
+				app.notFoundResponse(w, r, err)
+				return
+			default:
+				app.internalServerError(w, r, err)
+				return
+
+			}
+		}
+
+		ctx = context.WithValue(ctx, userCtx, user)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func getUserFromCtx(r *http.Request) *store.User {
+	user, ok := r.Context().Value(userCtx).(*store.User)
+	if !ok {
+		return nil
+	}
+	return user
 }
